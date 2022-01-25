922c02b88f82982bf3f39dc1a997cf201d47c45b
l3aobab <noelonazo@gmail.com>
2022-01-25 17:29:35 +0100

    show all and show one function
    Added functions to show all the passwords or show just one.
    
    Update function incomplete

--- keykeeper.py
+++ keykeeper.py
@@ -25,13 +25,40 @@ if ddbb:
 
 	createT="CREATE TABLE IF NOT EXISTS gestor (user_id int PRIMARY KEY AUTO_INCREMENT,sitio varchar(50) NOT NULL,usuario varchar(50),contraseña varchar(255) NOT NULL,email varchar(100))"
 	dbcursor.execute(createT)
-	#insertT="INSERT INTO gestor (sitio,usuario,contraseña,email) VALUES ('twitter','l3aobab','abc123.','correofalso@gmail.com')"
+	#insertT="INSERT INTO gestor (sitio,usuario,contraseña,email) VALUES ('instagram','l3aobab','abc.123','correofalso2@gmail.com')"
 	#dbcursor.execute(insertT)
 	#ddbb.commit()
 	
-	#consulta de prueba
-	showGestor="SELECT * FROM gestor"
-	dbcursor.execute(showGestor)
-	showGestorResultado=dbcursor.fetchall()
-	for fila in showGestorResultado:
+	#funcion para mostrar todas las contraseñas
+	def showAll():
+		showGestor="SELECT * FROM gestor"
+		dbcursor.execute(showGestor)
+		showGestorResultado=dbcursor.fetchall()
+		return showGestorResultado
+
+	#Hay que poner esto dentro del menu, opcion 1
+	selectAll=showAll()
+	for fila in selectAll:
 		print(fila)
+
+	#funcion para mostrar una linea en concreto
+	def showOne():
+		app=input("Indica una aplicación: ")
+		usr=input("Indica el nombre de usuario: ")
+		showGestorOne=("SELECT * FROM gestor WHERE sitio=%s and usuario=%s")
+		dbcursor.execute(showGestorOne,(app, usr, ))
+		sgor=dbcursor.fetchall()
+		return sgor
+
+	#Hay que poner esto dentro del menu, opcion 2
+	selectOne=showOne()
+	for fila in selectOne:
+		print(fila)
+
+	#completar esto mañana
+	def updateOne():
+		app=input("Indica una aplicación: ")
+		usr=input("Indica el nombre de usuario: ")
+		psw=input("Introduce la nueva contraseña: ")
+		psw2=input("Confirma la nueva contraseña: ")
+		if psw==psw2:
